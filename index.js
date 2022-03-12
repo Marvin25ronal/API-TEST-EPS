e9ebc3ac93bd96e40b2f5939b36b55012a741f47
Marvin <marvin1ronal@gmail.com>
2022-03-12 00:28:20 -0600

    nueva foto

--- index.js
+++ index.js
@@ -970,7 +970,7 @@ app.get('/userinfo', auth, (req, res) => {
         average: 89.9,
         document_id: '123456789',
         student_card: '987654321',
-        img: 'https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo='
+        img: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'
     }
     return res.status(200).json({
         success: true,
